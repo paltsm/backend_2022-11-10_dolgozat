ff06e8b5d83a890ea52ab3e79f8ea2edc2d7439e
misi <misipalotas@gmail.com>
2022-11-10 17:43:45 +0100

    felvetel fuggveny nem mukodik rendesen, vagy felveszi mindent vagy soha

--- src/index.js
+++ src/index.js
@@ -9,41 +9,81 @@ class Statue {
     }
 }
 let artworks = [];
+artworks.push(new Statue("test", 1234, 1234, 1234));
 function felvetel() {
-    // let aa = (document.getElementById('title') as HTMLInputElement).value;
-    // (document.getElementById('kimenet') as HTMLElement).textContent = aa;
-    title();
-    year();
-    price();
-    height();
+    let title = document.getElementById('title').value;
+    let year = parseInt(document.getElementById('year').value);
+    let price = parseInt(document.getElementById('price').value);
+    let height = parseInt(document.getElementById('height').value);
+    console.log("ssssssssssssssssssss");
+    // titlef();
+    // yearf();
+    // pricef();
+    // heightf();
+    if (titlef() && yearf() && pricef() && heightf()) {
+        artworks.push(new Statue(title, year, price, height));
+        for (let a of artworks) {
+            console.log(a);
+        }
+    }
+    else {
+        console.log("hulye");
+    }
 }
-function title() {
+function titlef() {
+    console.log("---title");
     let title = document.getElementById('title').value;
     if (title == null) {
         return false;
     }
     else {
         let reg = /[a-z ,-]+/g;
-        // console.log(reg.test(title))
+        console.log(reg.test(title));
+        console.log("t");
         return reg.test(title);
     }
 }
-function year() {
+function yearf() {
     console.log("---year");
     let year = parseInt(document.getElementById('year').value);
-    if (year > 2022) {
-        return false;
-    }
-    else {
-        console.log(year < 2022);
+    if (year < 2023) {
+        console.log(year < 2023);
+        console.log("y");
         return true;
     }
+    else {
+        console.log(year < 2023);
+        console.log("y");
+        return false;
+    }
 }
-function price() {
+function pricef() {
     console.log("---price");
+    let price = parseInt(document.getElementById('price').value);
+    if (price > 0) {
+        console.log(price > 0);
+        console.log("p");
+        return true;
+    }
+    else {
+        console.log(price > 0);
+        console.log("p");
+        return false;
+    }
 }
-function height() {
+function heightf() {
     console.log("---height");
+    let height = parseInt(document.getElementById('height').value);
+    if (height > 19) {
+        console.log(height > 19);
+        console.log("h");
+        return true;
+    }
+    else {
+        console.log(height > 19);
+        console.log("h");
+        return false;
+    }
 }
 function init() {
     var _a;
--- src/index.ts
+++ src/index.ts
@@ -15,41 +15,81 @@ class Statue implements Artwork{
 	
 }
 let artworks : Artwork[]=[];
+artworks.push(new Statue("test",1234,1234,1234))
 
 function felvetel() {
-	// let aa = (document.getElementById('title') as HTMLInputElement).value;
-	// (document.getElementById('kimenet') as HTMLElement).textContent = aa;
-	title();
-	year();
-	price();
-	height();
+	let title = (document.getElementById('title') as HTMLInputElement).value;
+	let year =parseInt((document.getElementById('year') as HTMLInputElement).value);
+	let price =parseInt((document.getElementById('price') as HTMLInputElement).value);
+	let height =parseInt((document.getElementById('height') as HTMLInputElement).value);
+	console.log("ssssssssssssssssssss");
+	// titlef();
+	// yearf();
+	// pricef();
+	// heightf();
+	if(titlef() && yearf() && pricef() && heightf()){
+		artworks.push(new Statue(title,year,price,height));
+		for(let a of artworks){
+			console.log(a);
+		}
+	}else{
+		console.log("hulye");
+	}
 }
-function title() : boolean{
+function titlef() : boolean{
+	console.log("---title")
 	let title = (document.getElementById('title') as HTMLInputElement).value;
 	if (title==null){
 		return false;
 	}else{
 		let reg:RegExp=/[a-z ,-]+/g;
-		// console.log(reg.test(title))
+		console.log(reg.test(title));
+		console.log("t");
 		return reg.test(title);
 
 	}
 }
-function year(){
+function yearf() : boolean{
 	console.log("---year")
 	let year =parseInt((document.getElementById('year') as HTMLInputElement).value);
-	if(year>2022){
-		return false;
-	}else{
-		console.log(year<2022);
+	
+	if(year<2023){
+		console.log(year<2023);
+		console.log("y");
 		return true;
+	}else{
+		console.log(year<2023);
+		console.log("y");
+		return false;
 	}
 }
-function price(){
+function pricef() : boolean{
 	console.log("---price")
+	let price =parseInt((document.getElementById('price') as HTMLInputElement).value);
+	
+	if(price>0){
+		console.log(price>0);
+		console.log("p");
+		return true;
+	}else{
+		console.log(price>0);
+		console.log("p");
+		return false;
+	}
 }
-function height(){
-	console.log("---height")
+function heightf() : boolean{
+	console.log("---height");
+	let height =parseInt((document.getElementById('height') as HTMLInputElement).value);
+	
+	if(height>19){
+		console.log(height>19);
+		console.log("h");
+		return true;
+	}else{
+		console.log(height>19);
+		console.log("h");
+		return false;
+	}
 }
 
 
